659250e21acaaf8c81132adde6486e5987bd46b6
Tibor Mandok <Mandok.Tibor@stud.vassboskovice.cz>
2024-05-06 12:34:58 +0200

    Přidejte soubory projektu.

---
+++ ConsoleApp1/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Dim cislo As Integer
+
+        Do
+            Console.Write("zadej cislo")
+            cislo = console.readline
+            console.writeline($"zadej cislo {cislo}")
+        Loop
+    End Sub
+End Module
